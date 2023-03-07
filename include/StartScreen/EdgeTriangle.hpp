bd280dff1b08eb5049b7be7d61eef788eb0c4be2
Onder BAYGIN <info@onderbaygin.com>
2023-03-08 00:40:09 +0300

    Abstract file structure

--- include/StartScreen/EdgeTriangle.hpp
+++ include/StartScreen/EdgeTriangle.hpp
@@ -1,13 +1,13 @@
 #ifndef START_SCREEN_EDGE_TRIANGLE_H
 #define START_SCREEN_EDGE_TRIANGLE_H
 
-#include "../Abstract/SDL_Image.hpp"
-#include "../Abstract/SDL_Rotatable.hpp"
+#include "../Abstract/SDL/Image.hpp"
+#include "../Abstract/SDL/Rotatable.hpp"
 
 namespace StartScreen
 {
-    class EdgeTriangle : public Abstract::SDL_Image,
-                         public Abstract::SDL_Rotatable
+    class EdgeTriangle : public Abstract::SDL::Image,
+                         public Abstract::SDL::Rotatable
     {
     public:
         ~EdgeTriangle();
--- include/StartScreen/MenuItem.hpp
+++ include/StartScreen/MenuItem.hpp
@@ -1,12 +1,12 @@
 #ifndef START_SCREEN_MENU_ITEM_H
 #define START_SCREEN_MENU_ITEM_H
 
-#include "../Abstract/SDL_Text.hpp"
+#include "../Abstract/SDL/Text.hpp"
 #include "../Abstract/Selectable.hpp"
 
 namespace StartScreen
 {
-    class MenuItem : public Abstract::SDL_Text,
+    class MenuItem : public Abstract::SDL::Text,
                      public Abstract::Selectable
     {
     };
