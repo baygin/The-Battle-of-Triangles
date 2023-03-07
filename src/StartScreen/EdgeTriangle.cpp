de2b77eec069526131fc0f07e388362d74fac274
Onder BAYGIN <info@onderbaygin.com>
2023-03-08 00:44:43 +0300

    Seg. Fault fixed

--- src/StartScreen/EdgeTriangle.cpp
+++ src/StartScreen/EdgeTriangle.cpp
@@ -15,5 +15,7 @@ StartScreen::EdgeTriangle::~EdgeTriangle()
 
 void StartScreen::EdgeTriangle::Render()
 {
-    SDL_RenderCopyEx(this->Renderer, this->Texture, NULL, &this->Rect, this->Angle, 0, SDL_FLIP_NONE);
+    if(this->Texture) {
+        SDL_RenderCopyEx(this->Renderer, this->Texture, NULL, &this->Rect, this->Angle, 0, SDL_FLIP_NONE);
+    }
 }
